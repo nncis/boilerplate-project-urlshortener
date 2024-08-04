2b1055dc50b42afa73ef7fd2c20de40dda349543
nncis <115885591+nncis@users.noreply.github.com>
2024-08-04 13:22:11 -0300

    1

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const url = require('url');
+const { MongoClient } = require('mongodb');
+const dns = require("dns");
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const mongoUri = process.env.MONGO_URI;
+
+const client = new MongoClient(mongoUri);
+const db = client.db('urlshortner');
+const urls = db.collection("urls");
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +31,40 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res) => {
+  const bodyUrl = req.body.url;
+  const parsedUrl = url.parse(bodyUrl, true).hostname;
+
+  const dnsLookUp = dns.lookup(parsedUrl, async(err, address) => {
+    if(!address){
+      res.json({ error: 'Invalid URL'})
+    } else {
+      const urlCount = await urls.countDocuments({});
+
+      const urlDoc = {
+        url: bodyUrl,
+        short_url: urlCount
+      };
+
+      const result = await urls.insertOne(urlDoc);
+      const response = {
+        original_url: bodyUrl,
+        short_url: urlCount
+      }
+      res.json(response)
+
+    }
+  })
+
+})
+
+app.get('/api/shorturl/:shorturl', async (req, res) => {
+  
+  const shorturl = req.params.shorturl;
+  const urlDoc = await urls.findOne({ short_url: +shorturl})
+  res.redirect(urlDoc.url)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
